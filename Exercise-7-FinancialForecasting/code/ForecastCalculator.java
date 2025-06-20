e991855cee1716ceaa994d9675ce4da3024aafea
Vutlapalli Venkata Suprathika Yashasri <2200030530cseh@gmail.com>
2025-06-20 22:13:12 +0530

    week1 added

--- Exercise-7-FinancialForecasting/code/ForecastCalculator.java
+++
@@ -1,9 +0,0 @@
-package finance;
-
-public class ForecastCalculator {
-
-    public static double calculateFutureValue(double presentValue, double growthRate, int years) {
-        if (years == 0) return presentValue;
-        return calculateFutureValue(presentValue * (1 + growthRate), growthRate, years - 1);
-    }
-}
